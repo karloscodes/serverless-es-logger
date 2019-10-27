9cea1f6f4e47c01415d09aef0d4067f3e8ed0983
Carlos Castellanos Vera <ccverak@gmail.com>
2019-10-27 12:33:02 +0100

    Some fixes

--- ElasticsearchStream.js
+++ ElasticsearchStream.js
@@ -1,7 +1,7 @@
 const Writable = require('stream').Writable
 const dayjs = require('dayjs')
 const defaultMappingTemplate = require('./index-template-mapping.json')
-const defaultTransformer = require('./lambdaLogTransformer')
+const defaultTransformer = require('./LambdaLogTransformer')
 
 function generateIndexName (pattern, entry) {
   const [prefix, datePattern] = pattern.split('-')
